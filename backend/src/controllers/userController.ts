207ae55a21f2999b36931856a590e68119dc14b4
Divyad007 <pavansoft007@gmail.com>
2026-05-16 20:26:40 +0530

    created register api

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -9,3 +9,17 @@ export const getusers = async (req:Request,res:Response) => {
    res.status(500).json({message:'server error'});
   }
 };
+
+export const registeruser = async (req:Request,res:Response) => {
+  try{
+    const {name,email,password} = req.body;
+   const result = await pool.query(`INSERT INTO users (name,email,password) values($1,$2,$3)`,[name,email,password]);
+   if(result.rowCount){
+    res.status(201).json({message:"Added user successfully."});
+   }
+  }catch(error){
+    res.status(500).json({
+      message : "server error"
+    });
+  }
+}
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import {getusers} from "../controllers/userController";
+import { getusers, registeruser } from "../controllers/userController";
 
 const route = express.Router();
 route.get("/users", getusers);
+route.post("/register", registeruser);
 export default route;
