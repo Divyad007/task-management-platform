2735a18c1bfc6635d5f1d6f518e1764b192f81ba
Divyad007 <divyadoddi007@gmail.com>
2026-05-16 20:38:21 +0530

    create register api

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,5 +3,6 @@ import { getusers, registeruser } from "../controllers/userController";
 
 const route = express.Router();
 route.get("/users", getusers);
+//added register api
 route.post("/register", registeruser);
 export default route;
