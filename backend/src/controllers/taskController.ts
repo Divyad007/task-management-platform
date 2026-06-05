e586e1f4e1cf949191bee55814f2ed34022eb165
Divyad007 <divyadoddi007@gmail.com>
2026-06-05 22:17:32 +0530

    created crud apis

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -80,12 +80,43 @@ export const updateTask = async (req: Request, res: Response) => {
       });
     } else {
       const taskUpdate = await pool.query(
-        `UPDATE tasks SET title=$2,description=$3,status=$4,points=$5 WHERE id=$1 AND user_id = $6`,
+        `UPDATE tasks SET title=$2,description=$3,status=$4,points=$5,updated_at = now() WHERE id=$1 AND user_id = $6`,
         [taskId, title, description, status, points, userId],
       );
       if (taskUpdate) {
         return res.status(200).json({
           message: "Tasks Update successfully",
+        });
+      }
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "server error",
+      err: error,
+    });
+  }
+};
+
+export const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const userId = (req as any).user.id;
+    const { taskId} = req.params;
+    const result = await pool.query(
+      `SELECT * FROM tasks where user_id = $1 AND id = $2`,
+      [userId, taskId],
+    );
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        message: "Tasks not Found.",
+      });
+    } else {
+      const taskdelete = await pool.query(
+        `DELETE FROM tasks WHERE id=$1 AND user_id = $2`,
+        [taskId, userId],
+      );
+      if (taskdelete) {
+        return res.status(200).json({
+          message: "Tasks Removed successfully",
         });
       }
     }
--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {tasks,getuserTasks,getTask,updateTask} from "../controllers/taskController";
+import {tasks,getuserTasks,getTask,updateTask,deleteTask} from "../controllers/taskController";
 import {verifytoken} from "../middleware/authMiddleware";
 
 const route = express.Router();
@@ -7,4 +7,5 @@ route.post("/tasks",verifytoken,tasks);
 route.get("/getuserTasks",verifytoken,getuserTasks);
 route.get("/getTask",verifytoken,getTask);
 route.put("/updateTask/:taskId",verifytoken,updateTask);
+route.delete("/deleteTask/:taskId",verifytoken,deleteTask);
 export default route;
